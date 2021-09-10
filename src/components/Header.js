c9b067c2c84751bf3147d2d8078e9e5c5887a335
ArcOmotola <ogunsola40@gmail.com>
2021-09-10 23:32:59 +0100

    added redux and deleted unwanted boilerplates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import logo from "../images/images/logo.svg";
 import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
 import CloseRoundedIcon from '@material-ui/icons/CloseRounded';
+import { selectCars } from '../features/car/carSlice';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+    const [burgerStatus, setBurgerStatus] = useState(false);
+    const cars = useSelector(selectCars);
+    
     return (
         <Container>
             <a>
                 <img src={logo} alt=""/>
             </a>
             <Menu>
-                <a href="#">Model S</a>
-                <a href="#">Model 3</a>
-                <a href="#">Model X</a>
-                <a href="#">Model Y</a>
+                {cars && cars.map((car, index) => (
+                    <a key={index} href="#">{car}</a>
+                ))}
             </Menu>
             <RightMenu>
                 <a href="#">Shop</a>
                 <a href="#">Tesla Account</a>
-                <CustomMenu />
+                <CustomMenu onClick={() => setBurgerStatus(true)}/>
             </RightMenu>
-            <BurgerNav>
+            <BurgerNav show={burgerStatus}>
                 <CloseWrapper>
-                    <CustomClose />
+                    <CustomClose onClick={() => setBurgerStatus(false)}/>
                 </CloseWrapper>
+                {cars && cars.map((car, index) => (
+                    <li key={index}><a href="#">{car}</a></li>
+                ))}
                 <li><a href="#">Existing Inventory</a></li>
                 <li><a href="#">Used Inventory</a></li>
                 <li><a href="#">Trade-in</a></li>
                 <li><a href="#">Cybertruck</a></li>
                 <li><a href="#">Roadster</a></li>
-                <li><a href="#">Existing Inventory</a></li>
-                <li><a href="#">Existing Inventory</a></li>
-                <li><a href="#">Existing Inventory</a></li>
             </BurgerNav>
             
         </Container>
@@ -97,6 +101,8 @@ const BurgerNav = styled.div`
     display: flex;
     flex-direction: column;
     text-align: start;
+    transform: ${props => props.show ? "translateX(0)":"translateX(100%)"};
+    transition: transform 0.2s ease-in;
     li {
         padding: 15px 0;
         border-bottom: 1px solid rgba(0, 0, 0, .2);
